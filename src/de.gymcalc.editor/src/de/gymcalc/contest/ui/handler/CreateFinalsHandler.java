03b325e4000c529aebd977634c8aa59fd8227778
groegert <thomas@groeger-clan.de>
2018-09-16 21:53:22 +0200

    juriclient: added 32-bit targets

--- src/de.gymcalc.editor/src/de/gymcalc/contest/ui/handler/CreateFinalsHandler.java
+++ src/de.gymcalc.editor/src/de/gymcalc/contest/ui/handler/CreateFinalsHandler.java
@@ -1,16 +1,15 @@
 package de.gymcalc.contest.ui.handler;
 
-import java.util.Collection;
-
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.jface.viewers.IStructuredSelection;
 
 import de.gymcalc.contest.command.CreateFinalsCommand;
 
 public class CreateFinalsHandler extends CommandHandler {
 
 	@Override
-	protected Command createCommand(EditingDomain domain, Collection<?> selection) {
-		return CreateFinalsCommand.create( domain, selection );
+	protected Command createCommand(EditingDomain domain, IStructuredSelection selection) {
+		return CreateFinalsCommand.create( domain, selection.toList() );
 	}
 }
